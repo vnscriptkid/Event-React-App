aa6d14fdf5fc101aea61e279ade0169b11a22583
Thanh Nguyen <vnscriptkid@gmail.com>
2019-10-05 12:49:24 +0700

    Add update trigger firestore function to listen for event cancelled

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,5 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-import { DocumentSnapshot } from 'firebase-functions/lib/providers/firestore';
 import {
   Activity,
   Event,
@@ -12,19 +11,47 @@ import { Timestamp } from '@firebase/firestore-types';
 admin.initializeApp(functions.config().firebase);
 
 export const addEventActivity = functions.firestore
-  .document(`events/${event}`)
-  .onCreate((event: DocumentSnapshot, context) => {
+  .document(`events/{event}`)
+  .onCreate((event, context) => {
     const newEvent = event.data() as Event;
     console.log({ newEvent });
     const activity: Activity = {
       type: ActivityType.NewEvent,
-      eventId: newEvent.id as string,
+      eventId: event.id,
       hostUid: newEvent.hostUid as string,
       hostedBy: newEvent.hostedBy,
       photoURL: newEvent.hostPhotoURL,
       timestamp: admin.firestore.FieldValue.serverTimestamp() as Timestamp,
       title: newEvent.title
     };
+    console.log({ activity });
+    return admin
+      .firestore()
+      .collection(`activities`)
+      .add(activity)
+      .then(docRef => console.log({ docRef }))
+      .catch(err => console.log({ err }));
+  });
+
+export const cancelledEventActivity = functions.firestore
+  .document(`events/{event}`)
+  .onUpdate((change, context) => {
+    const oldEvent = change.before.data() as Event;
+    const updatedEvent = change.after.data() as Event;
+    if (!oldEvent || !updatedEvent) return false;
+    if (oldEvent.cancelled || !updatedEvent.cancelled) return false;
+    console.log({ updatedEvent });
+
+    const activity: Activity = {
+      type: ActivityType.CancelEvent,
+      eventId: change.after.id,
+      hostUid: updatedEvent.hostUid as string,
+      hostedBy: updatedEvent.hostedBy,
+      photoURL: updatedEvent.hostPhotoURL,
+      timestamp: admin.firestore.FieldValue.serverTimestamp() as Timestamp,
+      title: updatedEvent.title
+    };
+
     console.log({ activity });
     return admin
       .firestore()
