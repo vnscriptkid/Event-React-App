447ac8ec2c95601ea2bff0f43912af961240c47b
Thanh Nguyen <vnscriptkid@gmail.com>
2022-04-07 00:33:50 +0700

    feat(config): configs

---
+++ src/config/index.ts
@@ -0,0 +1,14 @@
+export const EVENTS_PAGINATION = 10;
+export const GOOGLE_API_KEY = "AIzaSyBScF6081i8FsfeSBZVFW-394rmAPzjhps";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB_oIQI0v4k68-EN3IN1p-gZS9gwytcZpY",
+  authDomain: "events-d5ad0.firebaseapp.com",
+  databaseURL:
+    "https://events-d5ad0-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "events-d5ad0",
+  storageBucket: "events-d5ad0.appspot.com",
+  messagingSenderId: "711084899436",
+  appId: "1:711084899436:web:db0c9b65ca40c3f51cec49",
+  measurementId: "G-LSLDZXG27C",
+};
