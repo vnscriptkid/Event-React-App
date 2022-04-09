18680b994c8d1be13d0f363749fba3e74979722f
Thanh Nguyen <vnscriptkid@gmail.com>
2022-04-09 21:51:18 +0700

    refactor(async-action): simplify

--- src/features/async/asyncReducer.ts
+++ src/features/async/asyncReducer.ts
@@ -1,49 +1,109 @@
-import { AsyncAction } from './asyncActions';
-import { AsyncActionType } from './asyncConstants';
+const ASYNC_ACTION_START = "ASYNC_ACTION_START";
+const ASYNC_ACTION_FINISH = "ASYNC_ACTION_FINISH";
+const ASYNC_ACTION_ERROR = "ASYNC_ACTION_ERROR";
+export const APP_LOADED = "APP_LOADED";
 
-const initialState = {};
+export function asyncActionStart() {
+  return {
+    type: ASYNC_ACTION_START,
+  };
+}
 
-// fetchEvent_456 or fetchEvents
-export const createAsyncId = (payload: {
-  actionName: string;
-  actionId?: string;
-}) => {
-  const { actionId, actionName } = payload;
-  if (!actionName) throw new Error('Please provide async identifier');
-  let asyncId = actionName;
-  if (actionId) asyncId += `_${actionId}`;
-  return asyncId;
+export function asyncActionFinish() {
+  return {
+    type: ASYNC_ACTION_FINISH,
+  };
+}
+
+export function asyncActionError(error: any) {
+  console.log(error);
+  return {
+    type: ASYNC_ACTION_ERROR,
+    payload: error,
+  };
+}
+
+const initialState = {
+  loading: false,
+  error: null,
+  initialized: false,
 };
 
-export const asyncReducer = (state = initialState, action: AsyncAction) => {
-  switch (action.type) {
-    case AsyncActionType.Start:
-      const asyncIdStart = createAsyncId(action.payload);
+export function asyncReducer(state = initialState, { type, payload }: any) {
+  switch (type) {
+    case ASYNC_ACTION_START:
       return {
         ...state,
-        [asyncIdStart]: {
-          loading: true
-        }
+        loading: true,
+        error: null,
       };
-    case AsyncActionType.Finish:
-      const asyncIdFinish = createAsyncId(action.payload);
+    case ASYNC_ACTION_FINISH:
       return {
         ...state,
-        [asyncIdFinish]: {
-          loading: false
-        }
+        loading: false,
       };
-    case AsyncActionType.Error:
-      const asyncIdError = createAsyncId(action.payload);
+    case ASYNC_ACTION_ERROR:
       return {
         ...state,
-        [asyncIdError]: {
-          loading: false
-        }
+        loading: false,
+        error: payload,
       };
+    case APP_LOADED:
+      return {
+        ...state,
+        initialized: true,
+      };
+    default:
+      return state;
   }
-  return state;
-};
+}
+
+// import { AsyncAction } from './asyncActions';
+// import { AsyncActionType } from './asyncConstants';
+
+// const initialState = {};
+
+// // fetchEvent_456 or fetchEvents
+// export const createAsyncId = (payload: {
+//   actionName: string;
+//   actionId?: string;
+// }) => {
+//   const { actionId, actionName } = payload;
+//   if (!actionName) throw new Error('Please provide async identifier');
+//   let asyncId = actionName;
+//   if (actionId) asyncId += `_${actionId}`;
+//   return asyncId;
+// };
+
+// export const asyncReducer = (state = initialState, action: AsyncAction) => {
+//   switch (action.type) {
+//     case AsyncActionType.Start:
+//       const asyncIdStart = createAsyncId(action.payload);
+//       return {
+//         ...state,
+//         [asyncIdStart]: {
+//           loading: true
+//         }
+//       };
+//     case AsyncActionType.Finish:
+//       const asyncIdFinish = createAsyncId(action.payload);
+//       return {
+//         ...state,
+//         [asyncIdFinish]: {
+//           loading: false
+//         }
+//       };
+//     case AsyncActionType.Error:
+//       const asyncIdError = createAsyncId(action.payload);
+//       return {
+//         ...state,
+//         [asyncIdError]: {
+//           loading: false
+//         }
+//       };
+//   }
+//   return state;
+// };
 
 // const initialState = {
 //   loading: false
